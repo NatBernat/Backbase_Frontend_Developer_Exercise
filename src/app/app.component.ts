22a8ead167e193ad223fb9cbc822cac2d5b072cc
NatBernat <bernat.arcusa@gmail.com>
2022-07-26 12:18:32 +0200

    Added api request attempt

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
+import { getRequest } from 'src/utils/getRequest';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+  styleUrls: ['./app.component.sass'],
 })
 export class AppComponent {
-  title = 'weather-app';
+  title = 'Weather app';
+  place = { lat: 41.3879, lon: 2.16992 };
+  data: any = getRequest(this.place);
 }
